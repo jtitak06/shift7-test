186b1753a316261438b516a0134ebbca7e36a340
Jeff Titak <jtitak06@yahoo.com>
2022-06-25 20:28:37 -0500

    mobile menu built

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
+/* Solutions */
 const solutionsTab = document.getElementById('solutions');
-const productsTab = document.getElementById('products');
-const automationTab = document.getElementById('automation');
-const educationTab = document.getElementById('education');
-const resourcesTab = document.getElementById('resources');
-const dropdownDesktopSolutions = document.getElementById('desktop-solutions');
-const dropdownDesktopProducts = document.getElementById('desktop-products');
 const solutionsList = document.querySelector('.solutions-list');
-const productsList = document.querySelector('.products-list');
-const dropdownDesktopAutomation = document.getElementById('desktop-automation');
-const dropdownDesktopEducation = document.getElementById('desktop-education');
-const dropdownDesktopResources = document.getElementById('desktop-resources');
+const dropdownDesktopSolutions = document.getElementById('desktop-solutions');
 const navBorderSolutions = document.getElementById('nav-border-solutions');
 const navTextSolutions = document.getElementById('nav-text-solutions');
+/* Products */
+const productsTab = document.getElementById('products');
+const productsList = document.querySelector('.products-list');
+const dropdownDesktopProducts = document.getElementById('desktop-products');
 const navBorderProducts = document.getElementById('nav-border-products');
 const navTextProducts = document.getElementById('nav-text-products');
+/* Automation */
+const automationTab = document.getElementById('automation');
+const dropdownDesktopAutomation = document.getElementById('desktop-automation');
 const navBorderAutomation = document.getElementById('nav-border-automation');
 const navTextAutomation = document.getElementById('nav-text-automation');
+/* Education */
+const educationTab = document.getElementById('education');
+const dropdownDesktopEducation = document.getElementById('desktop-education');
 const navBorderEducation = document.getElementById('nav-border-education');
 const navTextEducation = document.getElementById('nav-text-education');
+/* Resources */
+const resourcesTab = document.getElementById('resources');
+const dropdownDesktopResources = document.getElementById('desktop-resources');
 const navBorderResources = document.getElementById('nav-border-resources');
 const navTextResources = document.getElementById('nav-text-resources');
+/* Hamburger Menu */
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+/* Mobile Menu */
+const mobileMenu = document.querySelector('.mobile-menu');
+/* Hero Section */
+const heroSection = document.querySelector('.hero-section');
+/* CLose Button */
+const closeButton = document.querySelector('.close-button');
 
 solutionsTab.addEventListener("click", () => {
     /*Activate Solutions Tab*/
@@ -146,3 +159,14 @@ resourcesTab.addEventListener("click", () => {
     navTextEducation.classList.remove('active');
 });
 
+/*Display Menu on Mobile*/
+hamburgerMenu.addEventListener("click", () => {
+    mobileMenu.classList.add('active');
+    heroSection.classList.add('hide');
+})
+
+/*Close Mobile Menu*/
+closeButton.addEventListener("click", () => {
+    mobileMenu.classList.remove('active');
+    heroSection.classList.remove('hide');
+})
